818e2af58f942bf4f32740513929631ea4f726f1
dellasabrina-16 <dellasabrinaa16@gmail.com>
2025-08-29 09:33:44 +0700

    commit - tampilan admin

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customer.pelanggan');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/pesananmasuk', function () {
+    return view('admin.pesananmasuk');
+});
+
+Route::get('/produk', function () {
+    return view('admin.produk');
+});
+
+Route::get('/pesananselesai', function () {
+    return view('admin.pesananselesai');
+});
+
+Route::get('/stok', function () {
+    return view('admin.stok');
+});
+
+Route::get('/login', function () {
+    return view('customer.login');
 });
